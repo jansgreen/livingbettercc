46d7dc34ed1386570039360ac9088a2020f1572e
jansgreen <jansgreen@gmail.com>
2024-10-25 11:25:13 -0400

    deploy 01

--- livingbettercc/settings.py
+++ livingbettercc/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 if os.getenv("DJANGO_ENV") == "heroku":
-    ALLOWED_HOSTS = [' https://livingbettercc-ba2163025eea.herokuapp.com']
+    ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
