0698aec6ee5101f639c620927a031036a4a2b969
jansgreen <jansgreen@gmail.com>
2024-12-15 15:56:43 -0500

    falta integral el POS a la plataforma 00

--- staticfiles/js/javascripts.js
+++ staticfiles/js/javascripts.js
@@ -1,22 +1,2 @@
-console.log("Probando la funcionalidad del js")
 
-$(document).ready(function() {
-    $('.select2').select2({
-      placeholder: "select the permissions for this group",
-      class:"bg-success",
-    });
-  
-  });
-  
-  $(document).ready(function(){
-    
-    //Get the user string
-    var userString = $("#string1").val();
-    
-    //Capitalize the first letter of the string
-    userString = userString.substring(0,1).toUpperCase() + userString.substring(1);
-  
-    //Display the results
-    $("#results").text(userString);
-  
-  });
+console.log("Probando la funcionalidad del js")
