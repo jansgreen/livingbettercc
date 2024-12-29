fc7d2ac6c8bff4e6696cf3c4b3227ce89dc0c8f1
jansgreen <jansgreen@gmail.com>
2024-12-29 16:42:45 -0500

    menu grupo correjido

--- groups/context_processors/context_processor.py
+++ groups/context_processors/context_processor.py
@@ -1,19 +1,22 @@
+from django.urls import reverse
+
 def obtener_menu_groups(request):
-        if request.user.is_authenticated:
-            user_in_manager_group = (request.user.is_authenticated and request.user.groups.filter(name='Superusuario').exists() )
-            menu = [
-                {
-                    'nombre': 'Grupo',
-                    'url': '#',
-                    'submenus': []
-                }
-            ]
-            
-            if user_in_manager_group:
-                menu[0]['submenus'].append({'nombre': 'Crear Biografia', 'url': '/auth/edit_biography/'})
-                menu[0]['submenus'].append({'nombre': 'Perfil', 'url': '/auth/ProfileFunction/'})
-                menu[0]['submenus'].append({'nombre': 'Editar Perfil', 'url': '/auth/edit_profile/'})
-            return {'menu_groups': menu}
-        else:
-            menu = None
-            return {'menu_groups': menu}
+    if request.user.is_authenticated:
+        user_in_manager_group = request.user.groups.filter(name='Superusuario').exists()
+        menu = [
+            {
+                'nombre': 'Grupo',
+                'url': '#',
+                'submenus': []
+            }
+        ]
+        
+        if user_in_manager_group:
+            menu[0]['submenus'].append({'nombre': 'Lista de Usuarios', 'url': reverse('user_list')})
+            menu[0]['submenus'].append({'nombre': 'Lista de Grupos', 'url': reverse('group_list')})
+            menu[0]['submenus'].append({'nombre': 'Crear Grupo', 'url': reverse('group_create')})
+            menu[0]['submenus'].append({'nombre': 'Invitar Amigo', 'url': reverse('invite_friend')})
+        
+        return {'menu_groups': menu}
+    else:
+        return {'menu_groups': None}
