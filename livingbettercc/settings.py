d5a569d9b2358c3f95ab8173c8e503691b25d5bf
jansgreen <jansgreen@gmail.com>
2026-01-01 01:32:36 -0500

    Fix media storage permanently 04

--- livingbettercc/settings.py
+++ livingbettercc/settings.py
@@ -248,14 +248,11 @@ if DATABASE_URL:
 else:
     DATABASES = {
             'default': dj_database_url.config(
-                default=os.getenv('DATABASE_URL'),
+                default=f"sqlite:///{BASE_DIR/'db.sqlite3'}",
                 conn_max_age=600,
                 ssl_require=True
             )
         }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
