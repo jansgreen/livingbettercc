141f7d8d09584eb8af36650ddd20063e10a485ef
jansgreen <jansgreen@gmail.com>
2026-02-07 09:06:55 -0500

    reparando errores de logs 02

--- shop/views.py
+++ shop/views.py
@@ -86,7 +86,7 @@ def category_create(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('category_list')
+            return redirect('shop:category_list')
     else:
         form = CategoryForm()
     return render(request, 'category_Shop_form.html', {'form': form})
@@ -98,7 +98,7 @@ def category_update(request, pk):
         form = CategoryForm(request.POST, instance=category)
         if form.is_valid():
             form.save()
-            return redirect('category_list')
+            return redirect('shop:category_list')
     else:
         form = CategoryForm(instance=category)
     return render(request, 'category_form.html', {'form': form})
@@ -108,7 +108,7 @@ def category_delete(request, pk):
     category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
         category.delete()
-        return redirect('category_list')
+        return redirect('shop:category_list')
     return render(request, 'category_confirm_delete.html', {'category': category})
 
 # View to add a product to the cart
